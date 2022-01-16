5f38bc14670ef9b70d7625cbdfb0f815b6fbbbd8
Cesar-Morales <moraleescesar@gmail.com>
2022-01-16 00:49:57 -0300

    Cambios [+] Delete terminado [+] Put terminado [ ] Agregar frontend

--- api-rest/src/routes/people.js
+++ api-rest/src/routes/people.js
@@ -1,9 +1,15 @@
+const e = require('express');
 const { Router } = require('express');
 const router = Router();
-const people = require('../sample.json'); // database simulation
+let people = require('../sample.json'); // database simulation
 
 router.get('/', (req, res) => {
     res.json(people);
+});
+
+//get a person by id
+router.get('/:id', (req, res) => {
+    res.json(people.find(person => person.id === parseInt(req.params.id)));
 });
 
 router.post('/', (req, res) => {
@@ -18,8 +24,37 @@ router.post('/', (req, res) => {
         people.push(newPerson);
         res.json(people);
     } else {
-        res.status(400).json({ error: 'Bad Request' });
+        res.status(500).json({ error: 'Bad Request' });
     }
 });
+
+//delete a person by id
+router.delete('/:id', (req, res) => {
+    console.log(req.params.id);
+    people = people.filter(person => person.id !== parseInt(req.params.id));
+    res.json(people);
+});
+
+//modify a person by id
+router.put('/:id', (req, res) => {
+    const { name, mail, rol } = req.body;
+    const person = people.find(person => person.id === parseInt(req.params.id));
+    if (!person) {
+        res.status(500).json({ error: 'Bad Request' });
+    } else {
+        if (name !== undefined) {
+            person.name = name;
+        }
+        if (mail !== undefined) {
+            person.mail = mail;
+        }
+        if (rol !== undefined) {
+            person.rol = rol;
+        }
+    }
+    people = people
+    res.json(people);
+});
+
 
 module.exports = router;
