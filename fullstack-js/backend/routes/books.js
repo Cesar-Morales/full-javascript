3d933c6935469756bfbebe7a8e04ba7e24893b3e
Cesar-Morales <moraleescesar@gmail.com>
2022-02-01 15:29:28 -0300

    backend js terminado. apirest lista (por ahora se le puede agregar mas cosas)

--- fullstack-js/backend/routes/books.js
+++ fullstack-js/backend/routes/books.js
@@ -8,4 +8,24 @@ router.get('/', async (req, res) => {
     res.json(books); 
 });
 
+router.post('/', async (req, res) => {
+    const {title, author, isbn} = req.body;
+    const newBook = new Book({title, author,isbn});
+    await newBook.save();
+    console.log(newBook);
+    res.json({ message: "Book recived" });
+});
+
+router.delete('/:id', async (req, res) => {
+    const _id = req.params.id;
+    const bookdeleted = await Book.findByIdAndDelete({_id});
+    res.json({  message: "Book deleted", 
+                Book : {
+                    title: bookdeleted.title,
+                    author: bookdeleted.author,
+                    isbn: bookdeleted.isbn
+                }
+            } );
+});
+
 module.exports = router;
