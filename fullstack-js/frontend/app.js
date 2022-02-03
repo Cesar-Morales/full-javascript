9486dd411f541c591ba96bd32a7ed9892e88db2c
Cesar-Morales <moraleescesar@gmail.com>
2022-02-03 16:54:04 -0300

    service terminado [+] agregando dir de img en bd [+] creando servicios [+] form terminado

--- fullstack-js/frontend/app.js
+++ fullstack-js/frontend/app.js
@@ -1,2 +1,27 @@
 require('./styles/app.css')
-//import './styles/app.css' //ecmascript 3
+//import './styles/app.css' //ecmascript 6
+//const BookService = require('./services/BookService')
+import BookService from './services/BookService'; //ecmascript 6
+
+document.getElementById('book-form')
+        .addEventListener('submit', function(e){
+            e.preventDefault();
+            var form = new FormData(this);
+            
+
+            const title = document.getElementById('title').value;
+            const author = document.getElementById('author').value;
+            const isbn = document.getElementById('isbn').value;
+            const image = document.getElementById('image').files;
+            
+            const formData = new FormData();
+            formData.append('title', title);
+            formData.append('author', author);
+            formData.append('isbn', isbn);
+            formData.append('image', image[0]);
+            
+            const bookService = new BookService();
+            bookService.postBook(formData);
+            
+        });
+
---
+++ fullstack-js/frontend/services/BookService.js
@@ -0,0 +1,37 @@
+// Servicio = Clase con metodos que podes usar en toda la app
+
+class BookService {
+
+    constructor() {
+        this.URI = 'http://localhost:3000/api/books';
+    }
+
+    getBooks() {
+        return fetch(this.URI)
+            .then(res => res.json())
+            .catch(err => console.log(err));
+    }
+    
+    postBook(book) {
+        return fetch(this.URI, {
+            method: 'POST',
+            body: book,
+        })
+            .then(res => res.json())
+            .catch(err => console.log(err));
+    }
+
+    deleteBook(id) {
+        return fetch(`${this.URI}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .catch(err => console.log(err));
+    }
+
+}
+
+export default BookService;
